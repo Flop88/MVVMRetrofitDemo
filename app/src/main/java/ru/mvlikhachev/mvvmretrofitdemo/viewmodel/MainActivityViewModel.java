7c635c1c20ba08da0486c70db4132f9399c00e13
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-25 08:21:45 +0300

    77. Edit MainActivityViewModel

--- app/src/main/java/ru/mvlikhachev/mvvmretrofitdemo/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/ru/mvlikhachev/mvvmretrofitdemo/viewmodel/MainActivityViewModel.java
@@ -5,21 +5,51 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
+import ru.mvlikhachev.mvvmretrofitdemo.model.MovieDataSource;
+import ru.mvlikhachev.mvvmretrofitdemo.model.MovieDataSourceFactory;
 import ru.mvlikhachev.mvvmretrofitdemo.model.MovieRepo;
 import ru.mvlikhachev.mvvmretrofitdemo.model.Result;
+import ru.mvlikhachev.mvvmretrofitdemo.service.MovieApiService;
+import ru.mvlikhachev.mvvmretrofitdemo.service.RetrofitInstance;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
     private MovieRepo movieRepo;
+    private LiveData<MovieDataSource> movieDataSourceLiveData;
+    private Executor executor;
+
+    private LiveData<PagedList<Result>> pagedListLiveData;
 
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
 
         movieRepo = new MovieRepo(application);
+
+        MovieApiService movieApiService = RetrofitInstance.getService();
+        MovieDataSourceFactory movieDataSourceFactory =
+                new MovieDataSourceFactory(application, movieApiService);
+        movieDataSourceLiveData = movieDataSourceFactory.getMutableLiveData();
+
+        PagedList.Config config = new PagedList.Config.Builder()
+              .setEnablePlaceholders(true)
+              .setInitialLoadSizeHint(10)
+              .setPageSize(20)
+              .setPrefetchDistance(3)
+              .build();
+
+        executor = Executors.newCachedThreadPool();
+
+        pagedListLiveData = new LivePagedListBuilder<Long, Result>(movieDataSourceFactory, config)
+                .setFetchExecutor(executor)
+                .build();
     }
 
     public LiveData<List<Result>> getAllMovieData() {
@@ -27,4 +57,7 @@ public class MainActivityViewModel extends AndroidViewModel {
         return movieRepo.getMutableLiveData();
     }
 
+    public LiveData<PagedList<Result>> getPagedListLiveData() {
+        return pagedListLiveData;
+    }
 }
